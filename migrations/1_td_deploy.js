2fb2f8b5be6476a3dede0dd65bae45948acf1443
lilyan <lilyan.bastien@gmail.com>
2022-02-11 15:17:59 +0100

    13/20

--- migrations/1_td_deploy.js
+++ migrations/1_td_deploy.js
@@ -9,13 +9,22 @@ var MyERC20 = artifacts.require("MyERC20TD.sol");
 
 module.exports = (deployer, network, accounts) => {
     deployer.then(async () => {
-        await deployTDToken(deployer, network, accounts); 
-        await deployEvaluator(deployer, network, accounts); 
-        await setPermissionsAndRandomValues(deployer, network, accounts); 
+        //await deployTDToken(deployer, network, accounts); 
+        //await deployEvaluator(deployer, network, accounts);
+		await rinkebyToken(deployer, network, accounts);
+        //await setPermissionsAndRandomValues(deployer, network, accounts); 
         await deployRecap(deployer, network, accounts);
 		await Exercices(deployer, network, accounts);  
     });
 };
+
+async function rinkebyToken(deployer, network, accounts) {
+	Evaluator = await evaluator.at("0x89a2Faa44066e94CE6B6D82927b0bbbb8709eEd7"); 
+	TDToken = await TDErc20.at("0xc2269af51350796aF4F6D52e4736Db3A885F28D6");
+	dummyToken = await TDErc20.at("0xbc3b69d1abD5A39f55a9Ba50C7a2aDd933952123");
+	uniswapV2FactoryAddress = "0x5c69bee701ef814a2b6a3edd4b1652cb9cc5aa6f"
+	wethAddress = "0xc778417e063141139fce010982780140aa0cd5ab"
+}
 
 async function deployTDToken(deployer, network, accounts) {
 	TDToken = await TDErc20.new("TD-AMM-101","TD-AMM-101",web3.utils.toBN("20000000000000000000000000000"))
@@ -60,22 +69,40 @@ async function Exercices(deployer, network, accounts) {
 	console.log("Init balance : " + getBalance.toString());
 
 	//exo6-a
-	console.log("Exercice 6a----------------")
+	/* console.log("Exercice 6a----------------")
 	await Evaluator.ex6a_getTickerAndSupply({from: account})
 	const ticker = await Evaluator.readTicker(account)
 	const supply = await Evaluator.readSupply(account)
 	console.log("ticker " + ticker)
 	console.log("supply " + supply)
 	const balance_ex6a = await TDToken.balanceOf(account)
-	console.log("balance ex6a : " + balance_ex6a)
+	console.log("balance ex6a : " + balance_ex6a) */
 
 	//exo6-b
-	console.log("Exercice 6b ------------")
-	MyERC20 = await MyERC20.new(ticker, ticker, supply)
-	console.log("My ERC20 " + MyERC20.address)
-	await Evaluator.submitErc20(MyERC20.address, {from: account})
+	/* console.log("Exercice 6b ------------")
+	myERC20 = await MyERC20.new(ticker, ticker, supply)
+	console.log("My ERC20 " + myERC20.address)
+	await Evaluator.submitErc20(myERC20.address, {from: account})
 	await Evaluator.ex6b_testErc20TickerAndSupply({from: account})
 	const balance_ex6b = await TDToken.balanceOf(account)
-	console.log("balance_ex6b " + balance_ex6b)
+	console.log("balance_ex6b " + balance_ex6b)  */
+
+	/* TDToken 0xc2269af51350796aF4F6D52e4736Db3A885F28D6
+	dummyToken 0xbc3b69d1abD5A39f55a9Ba50C7a2aDd933952123
+	Evaluator 0x89a2Faa44066e94CE6B6D82927b0bbbb8709eEd7
+	Init balance : 6000000000000000000
+	Exercice 6a----------------
+	ticker UoTH4
+	supply 398390257000000000000000000
+	balance ex6a : 6000000000000000000
+	Exercice 6b ------------
+	My ERC20 0x47720b8b92755883f1AD9DaD32616cbBD03Ba843 */
+
+	//exo7
+	console.log("Exercice 7 ------------")
+	myERC20 = await MyERC20.at("0x47720b8b92755883f1AD9DaD32616cbBD03Ba843")
+	await Evaluator.ex7_tokenIsTradableOnUniswap({from: account})
+	const balance_ex7 = await TDToken.balanceOf(account)
+	console.log("balance_ex7 " + balance_ex7)
 
 }
